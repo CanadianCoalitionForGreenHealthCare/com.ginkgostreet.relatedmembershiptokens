79f8e9ae81666f7fb8ca7d499c101bba405a0f83
David Reedy Jr <davidrjr.konadave@gmail.com>
2019-03-07 16:47:51 -0600

    ready for testing

--- relatedmembershiptokens.php
+++ relatedmembershiptokens.php
@@ -4,6 +4,71 @@ require_once 'relatedmembershiptokens.civix.php';
 use CRM_Relatedmembershiptokens_ExtensionUtil as E;
 
 /**
+ * implements hook_civicrm_tokens().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_tokens/
+ */
+function relatedmembershiptokens_civicrm_tokens(&$tokens) {
+
+  $tokens['primarymembership'] = array(
+    'primarymembership.id' => ts('Primary Membership: Membership ID'),
+    'primarymembership.contact_id' => ts('Primary Membership: Contact ID'),
+    'primarymembership.contact_display_name' => ts('Primary Membership: Contact Display Name'),
+    'primarymembership.contact_address_block' => ts('Primary Membership: Contact Address Block'),
+    'primarymembership.contact_phone' => ts('Primary Membership: Contact Phone'),
+    'primarymembership.contact_email' => ts('Primary Membership: Contact Email'),
+  );
+}
+
+/**
+ * implements hook_civicrm_tokenValues().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_tokenValues/
+ */
+function relatedmembershiptokens_civicrm_tokenValues(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+
+  if (!empty($tokens['primarymembership'])) {
+    $contacts = implode(',', $cids);
+
+    $dao = CRM_Core_DAO::executeQuery("SELECT rc.id, pm.id AS membership_id, pm.contact_id, pc.display_name, a.street_address, a.supplemental_address_1, a.supplemental_address_2, a.supplemental_address_3, a.city, sp.abbreviation, a.postal_code, p.phone, e.email
+      FROM civicrm_contact rc
+      INNER JOIN civicrm_membership rm ON rm.contact_id = rc.id
+      INNER JOIN civicrm_membership pm ON pm.id = rm.owner_membership_id
+      INNER JOIN civicrm_contact pc ON pc.id = pm.contact_id
+      LEFT JOIN civicrm_address a ON a.contact_id = pc.id AND a.is_primary=1
+      LEFT JOIN civicrm_state_province sp ON sp.id = a.state_province_id
+      LEFT JOIN civicrm_phone p ON p.contact_id = pc.id AND p.is_primary=1
+      LEFT JOIN civicrm_email e ON e.contact_id = pc.id AND e.is_primary=1
+      WHERE rc.id IN ($contacts)
+    ");
+
+    while ($dao->fetch()) {
+      $address = array();
+      if ($dao->street_address) {
+        $address[] = $dao->street_address;
+      }
+      if ($dao->supplemental_address_1) {
+        $address[] = $dao->supplemental_address_1;
+      }
+      if ($dao->supplemental_address_2) {
+        $address[] = $dao->supplemental_address_2;
+      }
+      if ($dao->supplemental_address_3) {
+        $address[] = $dao->supplemental_address_3;
+      }
+      $address[] = ($dao->city ? "{$dao->city}, " : '') . ($dao->abbreviation ? "{$dao->abbreviation} " : '') . ($dao->postal_code ?: '');
+
+      $values[$dao->id]['primarymembership.id'] = $dao->membership_id;
+      $values[$dao->id]['primarymembership.contact_id'] = $dao->contact_id;
+      $values[$dao->id]['primarymembership.contact_display_name'] = $dao->display_name;
+      $values[$dao->id]['primarymembership.contact_address_block'] = implode("\n", $address);
+      $values[$dao->id]['primarymembership.contact_phone'] = $dao->phone;
+      $values[$dao->id]['primarymembership.contact_email'] = $dao->email;
+    }
+  }
+}
+
+/**
  * Implements hook_civicrm_config().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
